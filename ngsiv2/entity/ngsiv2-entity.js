fa1255a79131d0e9264bb05a699c466388c1f117
Jose Manuel Cantera <josemanuel.cantera@fiware.org>
2019-03-20 19:03:39 +0100

    Supporting keyValues mode and validation

--- ngsiv2/entity/ngsiv2-entity.js
+++ ngsiv2/entity/ngsiv2-entity.js
@@ -5,27 +5,54 @@ module.exports = function(RED) {
 
   const v2Prefix = "v2/entities";
 
+  function validate(config, msg) {
+    let out = true;
+    if(!config.endpoint) {
+      out = false;
+    }
+
+    if (!msg.payload || !msg.payload.trim()) {
+      out = false;
+    }
+
+    return out;
+  }
+
   function NgsiEntityNode(config) {
     RED.nodes.createNode(this, config);
     const node = this;
 
     node.on("input", async function(msg) {
+      if (!validate(config, msg)) {
+        msg.payload = null;
+        node.error('Node is not properly configured or no entity Id provided');
+        return;
+      }
+
       const entityId = msg.payload;
 
       const endpoint = config.endpoint;
       const service = config.service;
+      const mode = config.mode;
 
-      const resource = `${endpoint}/${v2Prefix}/${entityId}`;
+      let resource = `${endpoint}/${v2Prefix}/${entityId}`;
+
+      if (mode === 'keyValues') {
+        resource += `?options=${mode}`
+      }
+
+      let headers = Object.create(null);
+      if (service.trim()) {
+        headers['Fiware-Service'] = service;
+      }
 
       let response = null;
       try {
-        response = await http.get(resource, {
-          'Fiware-Service': service
-        });
+        response = await http.get(resource, headers);
       }
       catch(e) {
         msg.payload = null;
-        node.error(`Exception while retrieving entity: ${entityId}`, e);
+        node.error(`Exception while retrieving entity: ${entityId}: ` + e);
         return;
       }
 
