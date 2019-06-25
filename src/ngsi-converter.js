97b17f1a9c32eb8385855730439375f5b486bfc1
Bjarke Hou Kammersgaard <>
2019-06-25 13:32:36 +0200

    Changed name to converter, fixed bugs from pull-request coments, moved convert-method to own file and changed copyright. Still needs testing.

--- src/ngsi-converter.js
+++ src/ngsi-converter.js
@@ -1,23 +1,10 @@
-/**
- *
- *   NGSI Transformer Node
- *
- *   It transforms a NGSI(v2,LD) entity to the other NGSI(v2,LD) format.
- *   E.g. v2 --> LD || LD --> v2
- *
- *   Copyright (c) 2019 Alexandra Instituttet A/S
- *
- *   @author Bjarke Hou Kammersgaard
- *
- */
-
 const URI = require("uri-js");
 
 const LD_CONTEXT = 'https://schema.lab.fiware.org/ld/context';
 const ETSI_CORE_CONTEXT = 'https://uri.etsi.org/ngsi-ld/v1/ngsi-ld-core-context.jsonld';
 
 function ngsildUri(typePart, idPart) {
-  return 'urn:ngsi-ld:'+typePart+':'+idPart;
+  return 'urn:ngsi-ld:${typePart}:${idPart}';
 }
 
 function ldId(entityId, entityType) {
@@ -55,11 +42,6 @@ function ldObject(attributeName, entityId) {
   }
 
   return out;
-}
-
-function ldToV2(entity) {
-  // WIP
-  return null;
 }
 
 function v2ToLD(entity, node) {
@@ -149,41 +131,6 @@ function normalizeDate(dateStr) {
   return out;
 }
 
-module.exports = function(RED) {
-  function NgsiTransformerNode(config) {
-    RED.nodes.createNode(this, config);
-    const node = this;
-
-    node.on('input', function(msg) {
-
-      if(!msg.payload) {
-        node.error('No payload provided');
-      }
-
-      // Validate JSON format
-      let entity = msg.payload;
-
-      if(typeof msg.payload === 'string') {
-        try {
-          entity = JSON.parse(msg.payload);
-        } catch (SyntaxError) {
-          node.error('Input was not valid');
-        }
-      }
-
-      // Transform
-      let transformedEntity = null;
-      if(config.transformMode === 'v2ToLD') {
-        transformedEntity = v2ToLD(entity, node);
-      } else if(config.transformMode === 'ldToV2') {
-        transformedEntity = ldToV2(msg.payload);
-      }
-
-      // Send
-      msg.payload = transformedEntity;
-      node.send(msg);
-    });
-  }
-
-  RED.nodes.registerType('NGSI-Transformer', NgsiTransformerNode);
-};
+module.exports = {
+  v2ToLD
+}
